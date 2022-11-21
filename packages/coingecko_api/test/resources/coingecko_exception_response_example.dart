3b3f10d02efc7c1c59afa334e2e348421f7be50c
Александр Пепеляев <user@MacBook-Air-Aleksandr.local>
2022-11-21 18:54:20 +0800

    delete tests

--- packages/coingecko_api/test/resources/coingecko_exception_response_example.dart
+++
@@ -1,7 +0,0 @@
-const Map<String, dynamic> coingeckoExceptionResponseExample = {
-  "status": {
-    "error_code": 429,
-    "error_message":
-        "You've exceeded the Rate Limit. Please visit https://www.coingecko.com/en/api/pricing to subscribe to our API plans for higher rate limits."
-  }
-};
