afecf94f5735089a1839d9164faa662eda516c8b
Alexander Pepelyaev <alexander.pepelyaev@avotech.uz>
2023-05-05 13:10:48 +0800

    ref

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,6 @@ import 'package:crypto_portfolio/data/gecko_api/api/gecko_api_client.dart';
 import 'package:crypto_portfolio/data/hive_api/api/hive_api_client.dart';
 import 'package:flutter/material.dart';
 
-///TODO: общий портфельный виджет и детальная с полной историей
-///TODO: библиотека для графиков https://pub.dev/packages/syncfusion_flutter_charts
-
 Future<void> main() async {
   final HiveApiClient hiveApiClient = await HiveApiClient.getClient;
   final GeckoApiClient geckoApiClient = GeckoApiClient.getClient;
