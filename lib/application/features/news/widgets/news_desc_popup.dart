3c1cdccdc7bac23fe5913ba28014ebf2930894fc
Alexander Pepelyaev <alexander.pepelyaev@avotech.uz>
2023-10-01 20:32:37 +0800

    redesign help popup in news page

--- lib/application/features/news/widgets/news_desc_popup.dart
+++ lib/application/features/news/widgets/news_desc_popup.dart
@@ -14,7 +14,7 @@ class NewsDescPopup extends StatelessWidget {
         child: ConstrainedBox(
           constraints: BoxConstraints(
             maxWidth: MediaQuery.sizeOf(context).width - 40,
-            maxHeight: MediaQuery.sizeOf(context).height * 0.66,
+            maxHeight: MediaQuery.sizeOf(context).height * 0.8,
           ),
           child: SingleChildScrollView(
             child: Column(
@@ -35,7 +35,7 @@ class NewsDescPopup extends StatelessWidget {
                       bottomLeft: Radius.circular(radius),
                       bottomRight: Radius.circular(radius),
                     ),
-                    color: AppColors.grayLight,
+                    color: AppColors.white,
                   ),
                   child: Padding(
                     padding: const EdgeInsets.all(10.0),
@@ -48,35 +48,46 @@ class NewsDescPopup extends StatelessWidget {
                           style: AppStyles.bold20,
                           textAlign: TextAlign.center,
                         ),
-                        SizedBox(height: 5),
+                        SizedBox(height: 15),
                         Text(
                           context.localization.personalNewsDesc,
                           style: AppStyles.normal14,
                         ),
-                        Divider(height: 20),
+                        Divider(height: 40, color: AppColors.blackLight),
                         Text(
                           context.localization.viewInSource,
                           style: AppStyles.bold20,
                         ),
-                        SizedBox(height: 5),
+                        SizedBox(height: 15),
                         Text(
                           '1. ${context.localization.clickOnLink}',
                           style: AppStyles.normal14,
                         ),
                         SizedBox(height: 5),
-                        Image.asset(tapSourcePath),
-                        SizedBox(height: 5),
+                        ColorFiltered(
+                            colorFilter: ColorFilter.mode(
+                              Colors.black.withOpacity(0.1),
+                              BlendMode.srcOver,
+                            ),
+                            child: Image.asset(tapSourcePath)),
+                        SizedBox(height: 15),
                         Text(
                           '2. ${context.localization.clickOnTitle}',
                           style: AppStyles.normal14,
                         ),
                         SizedBox(height: 5),
-                        Image.asset(tapHeaderPath),
-                        SizedBox(height: 10),
+                        ColorFiltered(
+                          colorFilter: ColorFilter.mode(
+                            Colors.white.withOpacity(0.1),
+                            BlendMode.srcOver,
+                          ),
+                          child: Image.asset(tapHeaderPath),
+                        ),
+                        SizedBox(height: 15),
                         Center(
                           child: ElevatedButton(
                             style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all(AppColors.white),
+                              backgroundColor: MaterialStateProperty.all(AppColors.primary),
                             ),
                             onPressed: () {
                               Navigator.of(context).pop();
@@ -85,7 +96,7 @@ class NewsDescPopup extends StatelessWidget {
                               padding: const EdgeInsets.symmetric(horizontal: 30.0),
                               child: Text(
                                 context.localization.ok,
-                                style: AppStyles.normal14.copyWith(color: AppColors.blackLight),
+                                style: AppStyles.normal14.copyWith(color: AppColors.white),
                               ),
                             ),
                           ),
